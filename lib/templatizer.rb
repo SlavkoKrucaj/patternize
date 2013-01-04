0054ea9f4f45bde3e113d45d49bebe3717ed55a0
Slavko Krucaj <slavko@infinum.hr>
2013-01-04 18:16:56 +0100

    Upgraded templating

--- lib/templatizer.rb
+++ lib/templatizer.rb
@@ -1,48 +1,43 @@
-class Templatizer < Thor
-  include Thor::Actions
+class Templatizer
   
   attr_accessor :project_name, :pattern_to_replace, :github_repo
-
-  no_tasks {
-
-    def initialize(project_name = "TemplateProject", github_repo = "")
-      @project_name = project_name
-      @github_repo = github_repo
-      @pattern_to_replace = '@project_name'
-    end
   
-    def create_project
-      
-      system "git clone https://github.com/SlavkoKrucaj/TemplateProject " + @project_name
-      
-      Dir.glob("./" + @project_name + "/**/*").reverse.select {|p| p.include? @pattern_to_replace}.each do |path|
-             
-         old_path = File.dirname(path)
-         new_path = old_path + '/' + File.basename(path).gsub(@pattern_to_replace, @project_name)
+  def initialize(project_name = "TemplateProject", github_repo = "")
+    @project_name = project_name
+    @github_repo = github_repo
+    @pattern_to_replace = '@project_name'
+  end
 
-         File.rename(path, new_path)    
-         
-         if !File.directory? new_path
-           template = ERB.new(File.read(new_path)).result(binding)           
-           File.open(new_path, "w+") { |file| file.write(template) }
-        end
+  def create_project
+    
+    system "git clone https://github.com/SlavkoKrucaj/TemplateProject " + @project_name
+    
+    Dir.glob("./" + @project_name + "/**/*").reverse.select {|p| p.include? @pattern_to_replace}.each do |path|
+           
+       old_path = File.dirname(path)
+       new_path = old_path + '/' + File.basename(path).gsub(@pattern_to_replace, @project_name)
+
+       File.rename(path, new_path)    
+       
+       if !File.directory? new_path
+         template = ERB.new(File.read(new_path)).result(binding)           
+         File.open(new_path, "w+") { |file| file.write(template) }
       end
-      
-      say "Pushing to GitHub repo == #{@github_repo}"
-      
-      if (@github_repo.empty?)
-        system "rm -rf ./" + @project_name + "/.git"
-      else
-        Dir.chdir @project_name
-        system "pwd"
-        system "git remote set-url origin " + @github_repo
-        system "git add -A ."
-        system 'git commit -m "Initial commit"'
-        system 'git push origin master'
-        Dir.chdir ".."
-      end
-  
     end
-  }
+      
+    system "rm -rf ./" + @project_name + "/.git"
+    
+    if (@github_repo && !@github_repo.empty?)
+      Dir.chdir @project_name
+      system "git init"
+      system "cp " + PATTERNIZE_ROOT + "/templates/gitignore ./.gitignore"
+      system "git add -A ."
+      system 'git commit -m "Initial commit"'
+      system "git remote add origin " + @github_repo
+      system 'git push -u origin master'
+      Dir.chdir ".."
+    end
+
+  end
   
 end
